63b1dd024ef99a8df151fc3a921adee157dc3627
shahriar <shahriarkabir050@gmail.com>
2022-04-27 03:05:26 +0600

    nested mapping

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-
 import './App.css';
-import List from "./components/uniqueid/list";
-
+  const users = [
+{
+  fullname:"shahriar",
+  age:"27",
+  phone : [
+    {
+      home:"017425555",
+      office: "017524654"
+    }
+  ]
+},
+{
+  fullname:"kabir",
+  age:"27",
+  phone : [
+    {
+      home:"017425555",
+      office: "017524654"
+    }
+  ]
+}
+]
+//nested mapping
 function App() {
   return (
     <div className="App">
-      <List/>
+     { users.map((kebol,index)=> (<article>
+        <h3> {kebol.fullname}</h3>
+        <p>{kebol.age}</p>
+     </article>))}
+      
     </div>
   );
 }
-
 export default App;
